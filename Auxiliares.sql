a19d5c2a55fddf111531ece4451861e581b83c43
Samuel Paredes <135689277+Samol19@users.noreply.github.com>
2023-06-13 22:46:28 -0500

    Update Auxiliares.sql

--- Auxiliares.sql
+++ Auxiliares.sql
@@ -1,1 +1,23 @@
----crecer
+-- Sentencias ddl
+-- create
+-- alter
+-- drop
+
+-- Sentencias dml
+-- select
+-- insert
+-- update
+-- delete
+
+--Funciones de agregación
+--max/min
+--sum
+--count
+--avg
+
+--join : unir 2 tablas (usualmente x id)
+--function : RETORNA ALGO(recibe parametros y devuelve valores)
+--views : guardar una consulta
+--procedures (almacenao) :MUESTRA LA TABLA guarda una tabla (recibe parametros )
+--cursor : recorrer cada celda de la tabla
+--trigers : Se accione mediante un evento.
